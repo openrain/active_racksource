3389816fad76a8b11100ada7e7f3f2086c5e73ed
remi <remi@remitaylor.com>
2009-02-09 17:26:50 -0700

    meh, i'm not sure how i'm going to lay out & write specs yet - need to make some working examples

--- examples/rails/app/models/dog.rb
+++
@@ -1,2 +0,0 @@
-class Dog < ActiveRecord::Base
-end
--- examples/rails/config/routes.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :dogs
-
-  map.resources :dogs
-  map.root :controller => 'dogs'
-end
